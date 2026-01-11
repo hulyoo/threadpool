f5b6025abf2cf2dc162b772332366089d289e3c2
jl <3525914268@qq.com>
2026-01-11 21:59:51 +0800

    添加注释，用来表明问题所在

--- v1/test.cpp
+++ v1/test.cpp
@@ -37,13 +37,13 @@ int main()
     // pool.submit(std::make_shared<MyTask>(task1));
     // pool.submit(std::make_shared<MyTask>(task1));
     // pool.submit(std::make_shared<MyTask>(task1));
-    //Result res1 = pool.submit(std::make_shared<MyTask>(task1));
+    Result res1 = pool.submit(std::make_shared<MyTask>(task1));
     //Result res = pool.submit(std::make_shared<MyTask>(task1));
     
 
-    int num = res.get().cast_<int>();
+    //int num = res.get().cast_<int>();
 
-    std::cout<<"num = " <<num<<std::endl;
+    //std::cout<<"num = " <<num<<std::endl;
     getchar();
 
     return 0;
